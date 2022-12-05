c1a7005e4b4a56ac2e572bb71b328a934df67e95
Gokul Das M <Gokul@NTP-LAP-723.chn.nestgroup.net>
2022-12-05 21:49:57 +0530

    add and home added

--- src/main/java/com/nest/productapp_backend/controller/ProductController.java
+++ src/main/java/com/nest/productapp_backend/controller/ProductController.java
@@ -1,10 +1,20 @@
 package com.nest.productapp_backend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
+    @GetMapping("/")
+    public String HomePage(){
+        return "welcome to home page";
+    }
+    @PostMapping("/add")
+    public String AddProduct()
+    {
+        return "Add product page";
+    }
 
 
 }
