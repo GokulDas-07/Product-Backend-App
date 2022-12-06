544744ea0ce09b23be2694c604f003af31376416
Gokul Das M <Gokul@NTP-LAP-723.chn.nestgroup.net>
2022-12-06 16:19:06 +0530

    post

---
+++ src/main/java/com/nest/productapp_backend/model/Product.java
@@ -0,0 +1,109 @@
+package com.nest.productapp_backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue
+    private int id;
+    private int empid;
+    private String productname;
+    private String manufacture;
+    private String expiry;
+    private String brand;
+    private int price;
+    private String seller;
+    private String distributor;
+
+    public Product() {
+    }
+
+    public Product(int id, int empid, String productname, String manufacture, String expiry, String brand, int price, String seller, String distributor) {
+        this.id = id;
+        this.empid = empid;
+        this.productname = productname;
+        this.manufacture = manufacture;
+        this.expiry = expiry;
+        this.brand = brand;
+        this.price = price;
+        this.seller = seller;
+        this.distributor = distributor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getEmpid() {
+        return empid;
+    }
+
+    public void setEmpid(int empid) {
+        this.empid = empid;
+    }
+
+    public String getProductname() {
+        return productname;
+    }
+
+    public void setProductname(String productname) {
+        this.productname = productname;
+    }
+
+    public String getManufacture() {
+        return manufacture;
+    }
+
+    public void setManufacture(String manufacture) {
+        this.manufacture = manufacture;
+    }
+
+    public String getExpiry() {
+        return expiry;
+    }
+
+    public void setExpiry(String expiry) {
+        this.expiry = expiry;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getSeller() {
+        return seller;
+    }
+
+    public void setSeller(String seller) {
+        this.seller = seller;
+    }
+
+    public String getDistributor() {
+        return distributor;
+    }
+
+    public void setDistributor(String distributor) {
+        this.distributor = distributor;
+    }
+}
